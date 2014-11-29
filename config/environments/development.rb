2978c8b2b25d220ee84f74b8d49da44f55830ec3
Ahmed NAdar <ahmedfay@hotmail.com>
2014-11-29 14:21:48 -0500

    Add configuration for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,18 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  } ￼
+
+  # Send email in development mode.
+  config.action_mailer.perform_deliveries = true
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
