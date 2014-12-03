6cdd79e59dd353cad551c93563e5cfe9b7a95991
Ahmed <ahmedfay@hotmail.com>
2014-12-02 19:35:54 -0500

    Validate user input for start and end date

--- Rosetta/Statistical_Analysis/functions.rb
+++ Rosetta/Statistical_Analysis/functions.rb
@@ -1,15 +1,29 @@
 require 'readline'
 require 'date'
+
+# Earliest date for consistent data
+DATA_START_DATE ='2006-09-20'
+
+# Max number of days to retrive data, be kind to remote server
+MAX_DAYS = 7
+
 # ask the user to provide start and end date
 def query_user_for_date_range
   start_date, end_date = nil
 
-  puts "\nFirst, we need a start date:"
-  start_date = query_user_for_date
+  # untill we have both dates
+  until start_date && end_date
+    puts "\nFirst, we need a start date:"
+    start_date = query_user_for_date
 
-  puts "\nNext, we need an end date:"
-  end_date = query_user_for_date
+    puts "\nNext, we need an end date:"
+    end_date = query_user_for_date
 
+    if !date_range_valid?(start_date, end_date)
+      puts "Let's start again!"
+      start_date = end_date = nil
+    end
+  end
   return start_date, end_date # retunr both values as an array
 end
 
@@ -29,6 +43,32 @@ def query_user_for_date
     rescue ArgumentError
       puts "\nInvalid date format."
     end
+    # set the date to nothing unless the date is valid
+    date = nil unless date_valid?(date)
   end
   return date
+end
+
+
+# validation for single date either start or end date
+# that date should be in a range between start date and today
+def date_valid?(date)
+  valid_dates = Date.parse(DATA_START_DATE)..Date.today
+  if valid_dates.cover?(date)
+    return true
+  else
+    puts "\nDate must be after #{DATA_START_DATE} and before today."
+    return false
+  end
+end
+
+def date_range_valid?(start_date, end_date)
+  if start_date > end_date
+    puts "\n Start date mush be before end date"
+    return false
+  elsif start_date + MAX_DAYS < end_date
+     puts "\nNo more that #{MAX_DAYS} days. Be kind to the remote server."
+    return false
+  end
+  return true
 end
