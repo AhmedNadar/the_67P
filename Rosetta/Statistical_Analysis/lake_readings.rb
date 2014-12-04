3a9f1c23916aeadde442a669851d61d741da2e91
Ahmed Nadar <ahmedfay@hotmail.com>
2014-12-03 23:59:40 -0500

    Calculate our data

--- Rosetta/Statistical_Analysis/lake_readings.rb
+++ Rosetta/Statistical_Analysis/lake_readings.rb
@@ -12,7 +12,10 @@ start_date, end_date = query_user_for_date_range
 # puts start_date.strftime('%B %d, %Y')
 # puts end_date.strftime('%B %d, %Y')
 
-READING_TYPES.each do |type, label|
-  readings = get_readings_from_remote_server_for_dates(type, start_date, end_date)
-  puts "#{label}: " + readings.join(", ")
-end
+#READING_TYPES.each do |type, label|
+#  readings = get_readings_from_remote_server_for_dates(type, start_date, end_date)
+#  puts "#{label}: " + readings.join(", ")
+#end
+
+results = retrieve_and_calculate_results(start_date, end_date)
+puts results.inspect
